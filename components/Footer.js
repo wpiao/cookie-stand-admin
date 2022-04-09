94af38300deb0c99dd53819e27d70d342422f986
Wenhao Piao <wenhaopiao520@gmail.com>
2022-04-09 11:14:10 -0700

    Add Header and Footer components

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer(props) {
+  return (
+    <footer className="text-black-500 bg-emerald-500 p-4">&copy; 2021</footer>
+  )
+}
---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header(props) {
+  return (
+    <header className="text-black-500 bg-emerald-500 p-4">
+      <h1 className="text-3xl">Cookie Stand Admin</h1>
+    </header>
+  )
+}
